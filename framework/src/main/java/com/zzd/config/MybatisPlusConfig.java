4895a9956cd9a905212acb18a5d01fb8f8612b10
zhuzedan <1031155817@qq.com>
2022-09-23 21:53:27 +0800

    mybatisplus分页插件

---
+++ framework/src/main/java/com/zzd/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.zzd.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author :zzd
+ * @date : 2022/9/23
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 3.4.0之后版本
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
