08b026bc675ace1efd6781186ecf553b6ff3daba
zhuzedan <1031155817@qq.com>
2022-09-25 14:20:23 +0800

    delete .idea dir

---
+++ framework/src/main/java/com/zzd/utils/SecurityUtils.java
@@ -0,0 +1,37 @@
+package com.zzd.utils;
+
+import com.zzd.domain.entity.LoginUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * @author :zzd
+ * @date : 2022/9/25
+ */
+public class SecurityUtils
+{
+
+    /**
+     * 获取用户
+     **/
+    public static LoginUser getLoginUser()
+    {
+        return (LoginUser) getAuthentication().getPrincipal();
+    }
+
+    /**
+     * 获取Authentication
+     */
+    public static Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public static Boolean isAdmin(){
+        Long id = getLoginUser().getUser().getId();
+        return id != null && 1L == id;
+    }
+
+    public static Long getUserId() {
+        return getLoginUser().getUser().getId();
+    }
+}
